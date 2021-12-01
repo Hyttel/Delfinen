a83880aea7e23aed9d41b6d7768311c9a8702541
Shortytheman <shor7ty@gmail.com>
2021-12-01 14:03:06 +0100

    så virker fyttemøjet

--- src/Core/Main.java
+++ src/Core/Main.java
@@ -10,8 +10,5 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         Menu menu = new Menu();
         menu.runMenu();
-        Konkurrencestyring g = new Konkurrencestyring();
-
-        g.startKonkurrence();
     }
 }
